44ccabfce4236d509ec0f170bad6ee5fb0bb2097
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-28 15:51:36 -0500

    Feat(2-do_deploy_web_static.py): code was updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -43,7 +43,7 @@ def do_deploy(archive_path):
     if res.failed:
         return False
 
-    res = run('ln -s {}/{} /data/web_static/current'.format(path, filename))
+    res = run('ln -s {}{} /data/web_static/current'.format(path, filename))
     if res.failed:
         return False
     print('New version deployed!')
