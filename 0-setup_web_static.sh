166b50c37a596917032448f18c3e28de3504ee23
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-28 10:54:21 -0500

    Feat(0-setup_web_static.sh): code was commented

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 # Prepare your web servers
+
+## Update server
 sudo apt-get -y update
-#sudo apt-get upgrade -y
+
+## Install NGINX
 sudo apt-get install -y nginx
+
+## Creates directories
 sudo mkdir -p /data/web_static/shared /data/web_static/releases/test
+
+## Create Symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+## Change owner and group like ubuntu
 sudo chown -R ubuntu:ubuntu /data
+
+## Write Hello World in index
 echo "Hello World" > /data/web_static/releases/test/index.html
+
+## Add new configuration to NGINX
 data="\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "/server_name _;/ a \\$data" /etc/nginx/sites-available/default
-#sudo apt-get autoremove -y
+
+## Restart NGINX
 sudo service nginx restart
