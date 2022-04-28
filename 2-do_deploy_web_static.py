da1e338944ac5325ec9fda58afe692e045285aef
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-28 13:35:26 -0500

    Feat(2-do_deploy_web_static.py): print was removed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,7 +26,6 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_exc))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_exc))
-        print("New version deployed!")
         return True
     except:
         return False
