641a8a8ab09966158faa3f7b5ef45a38eee15115
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-04-28 17:19:29 -0500

    Feat(2-do_deploy_web_static.py): code was updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,6 +26,6 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, filename))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, filename))
+        return True
     except Exception:
         return False
-    return True
